8b541b3ee76a690fc4f0d4d1a7f1ed1fbd2fcfb2
snirvt <snirvt@gmail.com>
2021-11-24 20:49:04 +0200

    added gradient function

--- HW1/dense_layer.py
+++ HW1/dense_layer.py
@@ -1,17 +1,11 @@
-    
-    
-    
-    
-    
 import numpy as np
-
 class Dense():
     def __init__(self, input_dim, output_dim, activation):
         self.W = self.init_weights(output_dim, input_dim)
-        self.b = np.random.randn(output_dim, 1)
-        self.g_X = None
-        self.g_W = None
-        self.g_b = None
+        self.b = self.init_weights(output_dim, 1)
+        self.g_X = 0
+        self.g_W = 0
+        self.g_b = 0
         self.activation = activation
 
     def jacTMV_x(self, x, v):
@@ -21,25 +15,27 @@ class Dense():
         self.g_X =  self.W.T @ act_hadamard
         return self.g_X
 
-
     def jacTMV_b(self, x, v):
-        m = x.shape[1]
-        # m=1
         linear = self.linear(x)
         act_deriv = self.activation.deriv(linear)
         act_hadamard = np.multiply(act_deriv, v)
-        self.g_b =  np.sum((1 / m) * act_hadamard, axis=1, keepdims=True)
+        self.g_b =  np.sum(act_hadamard, axis=1, keepdims=True)
         return self.g_b
 
-
     def jacTMV_W(self, x, v):
+        m = x.shape[1]
         linear = self.linear(x)
         act_deriv = self.activation.deriv(linear)
         act_hadamard = np.multiply(act_deriv, v)
         self.g_W = act_hadamard @ x.T
         return self.g_W
 
-    # initializes the weights
+    def gradient(self,X,v):
+        self.jacTMV_W(X, v)
+        self.jacTMV_b(X, v)
+        self.jacTMV_x(X, v)
+        return self.g_X
+
     def init_weights(self, input_dim, output_dim):
         return np.random.randn(input_dim, output_dim) * np.sqrt(input_dim * output_dim)
     
