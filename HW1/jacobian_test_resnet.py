95f43f9792841a19f8a5cc72529e797201f9c931
snirvt <snirvt@gmail.com>
2021-11-25 03:05:20 +0200

    added residual neural network

--- HW1/jacobian_test_resnet.py
+++ HW1/jacobian_test_resnet.py
@@ -1,6 +1,9 @@
 
-
 from res_layer import ResLayer
+from neural_network import NeuralNetwork
+from losses import CrossEntropy
+
+
 from activations import Tanh
 import matplotlib.pyplot as plt
 import numpy as np
@@ -25,12 +28,9 @@ def jacobian_transpose_test_X():
         e_normalized_d = eps * normalized_d
         x_perturbatzia = x + e_normalized_d
         gx_d = np.dot(lin1(x_perturbatzia)[:,0:1].T, u)
-        # jackMV_x = lin1.jacTMV_x(x, u)
-        # jackMV_x =  lin1.jac_x_TMV(x,u)
-        jackMV_x =  lin1.jacTMV_x_batch(x,u)
-
+        jacMV_x =  lin1.jacTMV_x_batch(x,u)
         no_grad.append(np.abs(gx_d - gx).squeeze())
-        x_grad.append(np.abs(gx_d - gx - e_normalized_d.T @ jackMV_x).squeeze())
+        x_grad.append(np.abs(gx_d - gx - e_normalized_d.T @ jacMV_x).squeeze())
     l = range(eps_num)
     plt.plot(l, no_grad, label='First Order')
     plt.plot(l, x_grad, label='Second Order')
@@ -39,10 +39,10 @@ def jacobian_transpose_test_X():
     plt.ylabel('Error')
     plt.yscale('log')
     plt.legend()
-    plt.savefig('./figures/Jacobian_test_resnet_X.png', transparent=True, bbox_inches='tight', pad_inches=0)
+    # plt.savefig('./figures/Jacobian_test_resnet_X.png', transparent=True, bbox_inches='tight', pad_inches=0)
     plt.show()
 
-jacobian_transpose_test_X()
+# jacobian_transpose_test_X()
 
 ''' jacobian test X'''
 def jacobian_test_X():
@@ -97,7 +97,6 @@ def jacobian_transpose_test_b():
         lin1.b += e_normalized_d
         gx_d = np.dot(lin1(x)[:,0:1].T, u)
         lin1.b = deepcopy(org_b)
-        # jacTMV_b = lin1.jacTMV_b(x, u)
         jacTMV_b =lin1.jacTMV_b_batch(x,u)
         no_grad.append(np.abs(gx_d - gx).squeeze())
         x_grad.append(np.abs(gx_d - gx - e_normalized_d.T @ jacTMV_b).squeeze())
@@ -110,7 +109,7 @@ def jacobian_transpose_test_b():
     plt.xlabel('Iteration')
     plt.ylabel('Error')
     plt.legend()
-    plt.savefig('./figures/Jacobian_test_resnet_b.png', transparent=True, bbox_inches='tight', pad_inches=0)
+    # plt.savefig('./figures/Jacobian_test_resnet_b.png', transparent=True, bbox_inches='tight', pad_inches=0)
     plt.show()
 
 # jacobian_transpose_test_b()
@@ -168,7 +167,6 @@ def jacobian_transpose_test_W1():
         lin1.W1 += e_normalized_d
         gx_d = np.dot(lin1(x)[:,0:1].T, u)
         lin1.W1 = deepcopy(org_W1)
-        # jacTMV_W1 = lin1.jacTMV_W1_kron_trick(x, u)
         jacTMV_W1 = lin1.jacTMV_W1_batch(x, u)
         no_grad.append(np.abs(gx_d - gx).squeeze())
         x_grad.append(np.abs(gx_d - gx - e_normalized_d.flatten() @ jacTMV_W1.flatten()).squeeze())
@@ -176,12 +174,12 @@ def jacobian_transpose_test_W1():
     l = range(eps_num)
     plt.plot(l, no_grad, label='First Order')
     plt.plot(l, x_grad, label='Second Order')
-    plt.title('Jacobian transposed verification wrt bias')
+    plt.title('Jacobian transposed verification wrt W1')
     plt.yscale('log')
     plt.xlabel('Iteration')
     plt.ylabel('Error')
     plt.legend()
-    plt.savefig('./figures/Jacobian_test_resnet_W1.png', transparent=True, bbox_inches='tight', pad_inches=0)
+    # plt.savefig('./figures/Jacobian_test_resnet_W1.png', transparent=True, bbox_inches='tight', pad_inches=0)
     plt.show()
 
 # jacobian_transpose_test_W1()
@@ -212,7 +210,7 @@ def jacobian_test_W1():
     l = range(eps_num)
     plt.plot(l, no_grad, label='First Order')
     plt.plot(l, x_grad, label='Second Order')
-    plt.title('Jacobian transposed verification wrt Weights')
+    plt.title('Jacobian transposed verification wrt W1')
     plt.yscale('log')
     plt.xlabel('Iteration')
     plt.ylabel('Error')
@@ -229,7 +227,7 @@ def jacobian_test_W1():
 
 def jacobian_test_transpose_W2():
     d = np.random.rand(3, 2)
-    x = np.random.rand(3, 1) ## x must be 1 sample, since the gradient should only be for 1 sample (gx[:,0:1], gx_d[:,0:1]) 
+    x = np.random.rand(3, 1) 
     normalized_d = d / np.linalg.norm(d)
     eps_num = 20
     eps_vals = np.geomspace(0.5, 0.5 ** eps_num, eps_num)
@@ -245,22 +243,21 @@ def jacobian_test_transpose_W2():
         lin1.W2 += e_normalized_d
         gx_d = np.dot(lin1(x)[:,0:1].T, u)
         lin1.W2 = deepcopy(org_W)
-        # jacTMV_W2 = lin1.jacTMV_W2(x, u)
         jacTMV_W2 = lin1.jacTMV_W2_batch(x, u)
         no_grad.append(np.abs(gx_d - gx).squeeze())
         x_grad.append(np.abs(gx_d - gx - e_normalized_d.reshape(1,-1) @ jacTMV_W2.reshape(-1,1)).squeeze())
     l = range(eps_num)
     plt.plot(l, no_grad, label='First Order')
     plt.plot(l, x_grad, label='Second Order')
-    plt.title('Jacobian transposed verification wrt Weights')
+    plt.title('Jacobian transposed verification wrt W2')
     plt.yscale('log')
     plt.xlabel('Iteration')
     plt.ylabel('Error')
     plt.legend()
-    plt.savefig('./figures/Jacobian_test_resnet_W2.png', transparent=True, bbox_inches='tight', pad_inches=0)
+    # plt.savefig('./figures/Jacobian_test_resnet_W2.png', transparent=True, bbox_inches='tight', pad_inches=0)
     plt.show()
 
-jacobian_test_transpose_W2()
+# jacobian_test_transpose_W2()
 
 def jacobian_test_W2():
     d = np.random.rand(3, 2)
@@ -290,9 +287,191 @@ def jacobian_test_W2():
     plt.ylabel('Error')
     plt.legend()
     plt.show()
-    #
+    
+    
+    
+    
 
-# jacobian_test_W2()
+def jacobian_transpose_test_nn_X():
+    d = np.random.rand(2, 1)
+    x = np.random.rand(2, 1)
+    normalized_d = d / np.linalg.norm(d)
 
-# if __name__ == '__main__':
-#     jacobian_transpose_test_X()
+    eps_num = 20
+    eps_vals = np.geomspace(0.5, 0.5 ** eps_num, eps_num)
+    lin1 = NeuralNetwork([2,10,10,2], lr=0.1, type=ResLayer)
+
+    fx = lin1(x)[:,0:1]
+    u = np.random.randn(2, 1)
+    gx = np.dot(fx.T, u)
+    no_grad, x_grad = [], []
+    
+    
+    labels = np.random.randint(2, size=1)  
+    C = np.zeros((2,labels.size))          
+    C[labels,np.arange(labels.size)] = 1  
+    
+    loss = CrossEntropy()
+    fx_loss = loss(fx, C)
+
+    for eps in eps_vals:
+        e_normalized_d = eps * normalized_d
+        x_perturbatzia = x + e_normalized_d
+        # gx_d = np.dot(lin1(x_perturbatzia)[:,0:1].T, u)
+        fx_d = lin1(x_perturbatzia)
+        # jacMV_x =  lin1.jacTMV_x_batch(x,u)
+        fx_d_loss = loss(fx_d, C)
+        lin1.backprop(C, update=False)
+        jacMV_x = lin1.g_X[-1]
+        no_grad.append(np.abs(fx_d_loss - fx_loss))
+        x_grad.append(np.abs(fx_d_loss - fx_loss - e_normalized_d.ravel().T @ jacMV_x.ravel()))
+    l = range(eps_num)
+    plt.plot(l, no_grad, label='First Order')
+    plt.plot(l, x_grad, label='Second Order')
+    plt.title('Jacobian transposed neural network verification wrt X')
+    plt.xlabel('Iteration')
+    plt.ylabel('Error')
+    plt.yscale('log')
+    plt.legend()
+    # plt.savefig('./figures/Jacobian_test_resnet_X.png', transparent=True, bbox_inches='tight', pad_inches=0)
+    plt.show()
+
+# jacobian_transpose_test_nn_X()
+
+    
+    
+    
+    
+''' jacobian test NN W1'''
+def jacobian_test_nn_W1():
+    d = np.random.rand(10, 2)
+    x = np.random.rand(2, 1)
+    normalized_d = d / np.linalg.norm(d)
+
+    eps_num = 20
+    eps_vals = np.geomspace(0.5, 0.5 ** eps_num, eps_num)
+    lin1 = NeuralNetwork([2,10,10,2], lr=0.1, type=ResLayer)
+    fx = lin1(x)[:,0:1]
+    no_grad, x_grad = [], []
+
+    labels = np.random.randint(2, size=1)  
+    C = np.zeros((2,labels.size))         
+    C[labels,np.arange(labels.size)] = 1   
+
+    loss = CrossEntropy()
+    fx_loss = loss(fx, C)
+    W1 = deepcopy(lin1.model[0].W1)
+    
+    for eps in eps_vals:
+        e_normalized_d = eps * normalized_d
+        lin1.model[0].W1 = W1 + e_normalized_d
+        fx_d = lin1(x)
+        fx_d_loss = loss(fx_d, C)
+        lin1.model[0].W1 = deepcopy(W1)
+        lin1.backprop(C, update=False)
+        jacMV_W = lin1.g_W[-1][0]
+        no_grad.append(np.abs(fx_d_loss - fx_loss))
+        x_grad.append(np.abs(fx_d_loss - fx_loss - e_normalized_d.ravel().T @ jacMV_W.ravel()))
+    l = range(eps_num)
+    plt.plot(l, no_grad, label='First Order')
+    plt.plot(l, x_grad, label='Second Order')
+    plt.title('Jacobian transposed neural network verification wrt W1')
+    plt.xlabel('Iteration')
+    plt.ylabel('Error')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+    
+# jacobian_test_nn_W1()    
+    
+    
+    
+    
+    
+''' jacobian test NN W2'''
+def jacobian_test_nn_W2():
+    d = np.random.rand(2, 10)
+    x = np.random.rand(2, 1)
+    normalized_d = d / np.linalg.norm(d)
+
+    eps_num = 20
+    eps_vals = np.geomspace(0.5, 0.5 ** eps_num, eps_num)
+    lin1 = NeuralNetwork([2,10,10,2], lr=0.1, type=ResLayer)
+    fx = lin1(x)[:,0:1]
+    no_grad, x_grad = [], []
+
+    labels = np.random.randint(2, size=1)  
+    C = np.zeros((2,labels.size))         
+    C[labels,np.arange(labels.size)] = 1   
+
+    loss = CrossEntropy()
+    fx_loss = loss(fx, C)
+    W2 = deepcopy(lin1.model[0].W2)
+    
+    for eps in eps_vals:
+        e_normalized_d = eps * normalized_d
+        lin1.model[0].W2 = W2 + e_normalized_d
+        fx_d = lin1(x)
+        fx_d_loss = loss(fx_d, C)
+        lin1.model[0].W2 = deepcopy(W2)
+        lin1.backprop(C, update=False)
+        jacMV_W = lin1.g_W[-1][1]
+        no_grad.append(np.abs(fx_d_loss - fx_loss))
+        x_grad.append(np.abs(fx_d_loss - fx_loss - e_normalized_d.ravel().T @ jacMV_W.ravel()))
+    l = range(eps_num)
+    plt.plot(l, no_grad, label='First Order')
+    plt.plot(l, x_grad, label='Second Order')
+    plt.title('Jacobian transposed neural network verification wrt W2')
+    plt.xlabel('Iteration')
+    plt.ylabel('Error')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+    
+# jacobian_test_nn_W2()    
+      
+    
+    
+    
+''' jacobian test NN b'''
+def jacobian_test_nn_b():
+    d = np.random.rand(10, 1)
+    x = np.random.rand(2, 1)
+    normalized_d = d / np.linalg.norm(d)
+
+    eps_num = 20
+    eps_vals = np.geomspace(0.5, 0.5 ** eps_num, eps_num)
+    lin1 = NeuralNetwork([2,10,10,2], lr=0.1, type=ResLayer)
+    fx = lin1(x)[:,0:1]
+    no_grad, x_grad = [], []
+
+    labels = np.random.randint(2, size=1)  
+    C = np.zeros((2,labels.size))         
+    C[labels,np.arange(labels.size)] = 1   
+
+    loss = CrossEntropy()
+    fx_loss = loss(fx, C)
+    b = deepcopy(lin1.model[0].b)
+    
+    for eps in eps_vals:
+        e_normalized_d = eps * normalized_d
+        lin1.model[0].b = b + e_normalized_d
+        fx_d = lin1(x)
+        fx_d_loss = loss(fx_d, C)
+        lin1.model[0].b = deepcopy(b)
+        lin1.backprop(C, update=False)
+        jacMV_W = lin1.g_b[-1]
+        no_grad.append(np.abs(fx_d_loss - fx_loss))
+        x_grad.append(np.abs(fx_d_loss - fx_loss - e_normalized_d.ravel().T @ jacMV_W.ravel()))
+    l = range(eps_num)
+    plt.plot(l, no_grad, label='First Order')
+    plt.plot(l, x_grad, label='Second Order')
+    plt.title('Jacobian transposed neural network verification wrt b')
+    plt.xlabel('Iteration')
+    plt.ylabel('Error')
+    plt.yscale('log')
+    plt.legend()
+    plt.show()
+    
+jacobian_test_nn_b()    
+      
--- HW1/neural_network.py
+++ HW1/neural_network.py
@@ -2,15 +2,17 @@ import numpy as np
 from copy import deepcopy
 
 from dense_layer import Dense
+from res_layer import ResLayer
 from softmax_regression import SoftmaxRegression
 from activations import Tanh, Relu
 from losses import CrossEntropy
 
 class NeuralNetwork():
-    def __init__(self, dim_list, activation=Tanh(), lr=0.01):
+    def __init__(self, dim_list, activation=Tanh(), lr=0.01, type = Dense):
         self.dim_list = dim_list
         self.lr = lr
         self.activation = activation
+        self.type = type
         self.model = []
         self.X_list = []
 
@@ -18,11 +20,16 @@ class NeuralNetwork():
         self.init_model()
 
     
-    def init_model(self): # dim_list = [2,3,2]
-        for i in range(len(self.dim_list)-2):
-            self.model.append(Dense(self.dim_list[i], self.dim_list[i+1], self.activation))
-        self.model.append(SoftmaxRegression(self.dim_list[-2], self.dim_list[-1]))    
-        
+    def init_model(self): 
+        if self.type == Dense:
+            for i in range(len(self.dim_list)-2):
+                self.model.append(Dense(self.dim_list[i], self.dim_list[i+1], self.activation))
+            self.model.append(SoftmaxRegression(self.dim_list[-2], self.dim_list[-1])) 
+        if self.type == ResLayer:
+            for i in range(len(self.dim_list)-2):
+                self.model.append(ResLayer(self.dim_list[0], self.dim_list[i+1], self.activation))
+            self.model.append(SoftmaxRegression(self.dim_list[0], self.dim_list[-1])) 
+            
     def feed_forward(self, X):
         result = deepcopy(X)
         self.X_list = [result]
@@ -33,8 +40,13 @@ class NeuralNetwork():
     
         
     def step_layer(self, layer):
-        layer.W -= self.lr * layer.g_W
-        layer.b -= self.lr * layer.g_b
+        if isinstance(layer, Dense) or isinstance(layer, SoftmaxRegression):
+            layer.W -= self.lr * layer.g_W
+            layer.b -= self.lr * layer.g_b
+        if isinstance(layer, ResLayer):
+            layer.W1 -= self.lr * layer.g_W1
+            layer.W2 -= self.lr * layer.g_W2
+            layer.b -= self.lr * layer.g_b
         
     def init_gradient_tracks(self):
         self.g_X = []
@@ -57,10 +69,9 @@ class NeuralNetwork():
         
         for i in reversed(range(len(self.model)-1)):
             g_X = self.model[i].gradient(self.X_list[i], g_X)
-            _ = self.gradient_track(self.model[-1])
+            _ = self.gradient_track(self.model[i])
             if update:
                 self.step_layer(self.model[i])
-                
         
     def __call__(self, X):
         return self.feed_forward(X)
